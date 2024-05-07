e1d9e4750d3e3dd1b9a8b0270ed82df96a2f2505
Daltochan <thedaltochan@gmail.com>
2024-05-07 16:53:40 +0200

    Simple form added

--- resources/views/todoapp/index.blade.php
+++ resources/views/todoapp/index.blade.php
@@ -3,4 +3,11 @@
 
 @section("content")
 <h1>@yield("title")</h1>
+
+<form method="POST" action="#">
+    @csrf
+    <input type="text" name="task" placeholder="Enter task here">
+    <button type="submit">Send</button>
+</form>
+
 @endsection
